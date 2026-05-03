6f9844c8dddae42a0ed96f4662a23a8b2fee3528
Ivan Soldatov <mne21z@gmail.com>
2026-05-04 00:28:37 +0700

    dev: set max height for 'Configuration2' attribute value

--- PilotObjectInfo/Behaviors/CopyBehaviors/TypesAttrsCopyCellValueBehavior.cs
+++ PilotObjectInfo/Behaviors/CopyBehaviors/TypesAttrsCopyCellValueBehavior.cs
@@ -1,11 +1,59 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace PilotObjectInfo.Behaviors.CopyBehaviors
 {
     public class TypesAttrsCopyCellValueBehavior : CopyCellValueBehavior
     {
+        private static string GetCellDisplayText(FrameworkElement cellRoot)
+        {
+            if (cellRoot is TextBlock direct)
+            {
+                return direct.Text;
+            }
+
+            if (cellRoot is ContentPresenter presenter)
+            {
+                if (presenter.Content is TextBlock fromContent)
+                {
+                    return fromContent.Text;
+                }
+
+                var inTemplate = FindVisualChild<TextBlock>(presenter);
+                if (inTemplate != null)
+                {
+                    return inTemplate.Text;
+                }
+            }
+
+            var nested = FindVisualChild<TextBlock>(cellRoot);
+            return nested?.Text ?? string.Empty;
+        }
+
+        private static T FindVisualChild<T>(DependencyObject parent)
+            where T : DependencyObject
+        {
+            var count = VisualTreeHelper.GetChildrenCount(parent);
+            for (var i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                if (child is T match)
+                {
+                    return match;
+                }
+
+                var nested = FindVisualChild<T>(child);
+                if (nested != null)
+                {
+                    return nested;
+                }
+            }
+
+            return null;
+        }
+
         public override void OnPreviewKeyDownCustom(object sender, KeyEventArgs e)
         {
             if (sender is not DataGrid)
@@ -17,8 +65,15 @@ namespace PilotObjectInfo.Behaviors.CopyBehaviors
             if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.C)
             {
                 var currentCell = dataGrid.CurrentCell;
-                var content = currentCell.Column.GetCellContent(currentCell.Item) as TextBlock;
-                Clipboard.SetText(content.Text);
+                var content = currentCell.Column?.GetCellContent(currentCell.Item);
+                if (content != null)
+                {
+                    var text = GetCellDisplayText(content);
+                    if (text.Length > 0)
+                    {
+                        Clipboard.SetText(text);
+                    }
+                }
 
                 e.Handled = true;
             }
